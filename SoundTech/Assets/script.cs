da4897f67611c894eb9f8fb3708c84219c397866
Onnofon <Marcus.Bicker@gmail.com>
2021-01-03 20:26:55 +0100

    Edited normal version

---
+++ SoundTech/Assets/script.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour
+{
+
+    public void OpenLink()
+    {
+        Application.OpenURL("https://docs.google.com/forms/d/e/1FAIpQLSfbpmf3Rf7ztyhylxYh-DDFir7sNCnY301es2TceSLOxLy44Q/viewform?fbzx=2192976970102490640");
+    }
+
+}
